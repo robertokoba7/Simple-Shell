109e31f9ee267762e4e97746b918537e48bcc509
robertokoba7 <okobarobert7@gmail.com>
2022-08-16 19:48:58 +0300

    fix

--- main.c
+++ main.c
@@ -34,9 +34,9 @@ void set_data(data_shell *datash, char **av)
 	datash->counter = 1;
 
 	for (i = 0; environ[i]; i++)
-	{
 		;
 	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
 	for (i = 0; environ[i]; i++)
 		datash->_environ[i] = _strdup(environ[i]);
 	datash->_environ[i] = NULL;
